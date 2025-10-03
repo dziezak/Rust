fb3892cb78f59635f063c482c651c3028bfdd360
dziezak <01189082@pw.edu.pl>
2025-10-03 14:46:50 +0200

    projekt z wykladu dziala

--- Lab1/src/main.rs
+++ Lab1/src/main.rs
@@ -7,36 +7,31 @@ fn main() {
     loop{
         println!("Podaj liczbę!");
 
-        let mut line = String :: new();
+        let mut guess = String :: new();
         io::stdin()
-            .read_line(&mut line)
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
-        match line.trim().parse::<u64>(){
-            Ok(guess_x) => {
-                println!("Podano liczbe: {}", guess_x);
+        match guess.trim().parse::<u64>(){
+            Ok(guess) => {
+                println!("Your guess: {}", guess);
+                if guess == 0{
+                    println!("Your guess is 0. You forfited");
+                    std::process::exit(1);
+                }
+                match guess.cmp(&secret_number){
+                    Ordering::Less => println!("Too small!"),
+                    Ordering::Greater => println!("Too big!"),
+                    Ordering::Equal => {
+                        println!("You win!");
+                        std::process::exit(0);
+                    },
+                }
             }
             Err(_) => {
                 eprintln!("Blad: to nie jest liczba w u64");
                 std::process::exit(0);
             }
-        }
-        let guess_x: u64 = line.trim()
-            .parse()
-            .expect("Please type a number!");
-
-        println!("Your guess: {}", guess_x);
-        if(guess_x == 0){
-            println!("Your guess is 0. You forfited");
-            std::process::exit(1);
-        }
-        match guess_x.cmp(&secret_number){
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                std::process::exit(0);
-            },
         }
     }
 
