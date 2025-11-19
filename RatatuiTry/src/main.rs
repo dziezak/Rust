a49ddc15d0421f5a05d4df4497dcf01796ef9240
dziezak <01189082@pw.edu.pl>
2025-11-19 21:43:51 +0100

    just first ok looking ap

--- RatatuiTry/src/main.rs
+++ RatatuiTry/src/main.rs
@@ -1,49 +1,84 @@
-use ratatui::widgets::{Block, Paragraph};
-use color_eyre::Result;
+use std::io;
 use crossterm::event::{self, Event, KeyCode};
-use ratatui::{DefaultTerminal, Frame};
-//use ratatui::symbols::Marker::Block;
-use chrono::Local;
-use ratatui::widgets::Borders;
+use ratatui::buffer::Buffer;
+use ratatui::DefaultTerminal;
+use ratatui::layout::Rect;
+use ratatui::prelude::{Line, Text, Widget, Frame};
+use ratatui::style::Stylize;
+use ratatui::symbols::border;
+use ratatui::widgets::{Block, Paragraph};
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    let mut terminal = ratatui::init();
-    let result = run(terminal);
-    ratatui::restore();
-    result
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    let mut pressed = false;
-    let mut message:Option<String> = None;
-    loop {
-        terminal.draw(|f| {
-            let size = f.size();
-            let block = Block::default()
-                .title("Ratatui")
-                .borders(Borders::ALL);
-            f.render_widget(block, size);
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Counter App Tutorial ".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
 
-            if let Some(msg) = &message{
-               let paragraph = Paragraph::new(msg.clone())
-                   .block(Block::default().title("Message").borders(Borders::ALL));
-                f.render_widget(paragraph, size);
-            }
-        })?;
-        if let Event::Key(key) = event::read()? {
-            let now = Local::now();
-            message = Some(format!(
-                "You pressed {:?} at {}", key.code, now.format("%Y-%m-%d %H:%M:%S")
-            ));
-            if key.code == KeyCode::Char('q') {
-                break Ok(())
-            }
-        }
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
 
-fn render(frame: &mut Frame){
-    frame.render_widget("Hello world my darling", frame.area());
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Left => {
+                    if self.counter > 0 {
+                        self.counter -= 1;
+                    }
+                }
+                KeyCode::Right => {
+                    self.counter = self.counter.saturating_add(1);
+                }
+                KeyCode::Char('q') | KeyCode::Esc => {
+                    self.exit = true;
+                }
+                _ => {}
+            }
+        }
+        Ok(())
+    }
 }
 
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
