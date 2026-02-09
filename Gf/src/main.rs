9c1e5fd366a3873d5cc8415db0f91c7d554618e8
dziezak <01189082@pw.edu.pl>
2026-02-09 18:54:09 +0100

    hearts got added

--- Gf/src/main.rs
+++ Gf/src/main.rs
@@ -2,6 +2,14 @@
 use macroquad::prelude::*;
 use macroquad::prelude::ImageFormat;
 
+struct Serduszko {
+    pos: Vec2,
+    vel: Vec2,
+    life: f32,
+    size: f32,
+    rotation: f32,
+}
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "Niespodzianka!".to_owned(),
@@ -16,10 +24,42 @@ fn window_conf() -> Conf {
     }
 }
 
+fn obsluguj_czasteczki(czasteczki: &mut Vec<Serduszko>, mouse_vec: Vec2) {
+    if czasteczki.len() < 50 {
+        czasteczki.push(Serduszko {
+            pos: mouse_vec,
+            vel: vec2(rand::gen_range(-1.5, 1.5), rand::gen_range(-1.5, 1.5)),
+            life: 1.0,
+            size: rand::gen_range(15.0, 30.0),
+            rotation: rand::gen_range(0.0, 6.28),
+        });
+    }
+
+    for i in (0..czasteczki.len()).rev() {
+        let s = &mut czasteczki[i];
+        s.life -= 0.02;
+        s.pos += s.vel;
+
+        if s.life <= 0.0 {
+            czasteczki.remove(i);
+        } else {
+            let color = Color::new(1.0, 0.4, 0.6, s.life);
+            draw_text_ex("<3", s.pos.x, s.pos.y, TextParams {
+                font_size: s.size as u16,
+                color,
+                rotation: s.rotation,
+                ..Default::default()
+            });
+        }
+    }
+}
+
+
 #[macroquad::main(window_conf)]
 async fn main() {
     let kotek_bytes = include_bytes!("kotek.png");
     let kotek_texture = Texture2D::from_file_with_format(kotek_bytes, Some(ImageFormat::Png));
+    let mut czasteczki: Vec<Serduszko> = Vec::new();
 
     let mut nie_pos = vec2(450.0, 350.0);
     let mut nie_target = nie_pos;
@@ -85,6 +125,8 @@ async fn main() {
             draw_text("Hura! <3", 320.0, 250.0, 60.0, RED);
             draw_text("Gratulacje, zostalas moja Walentynka!", 130.0, 350.0, 40.0, BLACK);
         }
+
+        obsluguj_czasteczki(&mut czasteczki, mouse_vec);
 
         next_frame().await
     }
